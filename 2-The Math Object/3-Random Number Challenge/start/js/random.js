16eb30b891e061a8c0e79c5424d5f5e4ac07f50f
marsha <marshaannon@gmail.com>
2020-10-13 11:42:42 -0400

    exercise complete

--- 2-The Math Object/3-Random Number Challenge/start/js/random.js
+++ 2-The Math Object/3-Random Number Challenge/start/js/random.js
@@ -1,11 +1,17 @@
 // Collect input from a user
-
+const userInput = prompt("Please choose a number");
 
 // Convert the input to a number
-
+const userNumber = parseInt(userInput);
 
 // Use Math.random() and the user's number to generate a random number
-
-
 // Create a message displaying the random number
+if (userNumber) {
+    const randomNumber = Math.floor(Math.random() * userNumber) + 1;
+    console.log(`Your random number is: ${randomNumber}`);
+} else {
+    console.log("You must choose a number");
+}
+
+
 
